61d5a7c92e41e147f5b45ee437a35f21eab4eb0e
Ian Eure <ian.eure@gmail.com>
2010-05-26 09:27:29 -0700

    Remove example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the thrift-fb303 package
-#
-0 4	* * *	root	thrift-fb303_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(thrift-fb303):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="thrift-fb303" command="/usr/bin/thrift-fb303"
